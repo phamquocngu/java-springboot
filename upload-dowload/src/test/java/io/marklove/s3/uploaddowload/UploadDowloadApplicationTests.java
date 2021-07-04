0c1956e7561fe5d373533278c62c58c4967214d4
ngupq <phamquocngu.dt9@gmail.com>
2021-07-04 21:51:01 +0700

    upload-dowload using S3

---
+++ upload-dowload/src/test/java/io/marklove/s3/uploaddowload/UploadDowloadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.marklove.s3.uploaddowload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadDowloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
